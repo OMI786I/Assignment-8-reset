5f609ec4853a56b048223bde4fc60c3ff2fe1638
OMI786I <sohrawardy1998@gmail.com>
2024-05-22 04:36:10 +0600

    fixed data.find not a function error on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,29 @@
-import { Link, useLoaderData } from "react-router-dom";
-import LoadedData from "./LoadedData";
+import { Link } from "react-router-dom";
+import LoadingData from "./LoadingData";
 
 const Home = () => {
-  const data = useLoaderData();
-  console.log(data);
   return (
     <div>
+      <div className="hero min-h-screen bg-[#f2f2f2] md:px-16 rounded-xl">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="pngwing-1.png"
+            className=" max-w-xs md:max-w-sm rounded-lg "
+          />
+          <div>
+            <h1 className="text-5xl font-bold">
+              Books to freshen up your bookshelf
+            </h1>
+            <Link to="listedBooks">
+              <button className="btn btn-success text-white mt-16">
+                View The List
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
       <div>
-        <div className="hero min-h-screen bg-[#f2f2f2] md:px-16 rounded-xl">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img
-              src="pngwing-1.png"
-              className=" max-w-xs md:max-w-sm rounded-lg "
-            />
-            <div>
-              <h1 className="text-5xl font-bold">
-                Books to freshen up your bookshelf
-              </h1>
-              <Link to="listedBooks">
-                <button className="btn btn-success text-white mt-16">
-                  View The List
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="mt-10">
-          <h1 className=" text-center text-4xl">Books</h1>
-          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-7">
-            {data.map((data2) => (
-              <LoadedData key={data2.bookId} data2={data2}></LoadedData>
-            ))}
-          </div>
-        </div>
+        <LoadingData></LoadingData>
       </div>
     </div>
   );
---
+++ src/components/LoadingData.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import LoadedData from "./LoadedData";
+
+const LoadingData = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  });
+
+  return (
+    <div>
+      <div className="mt-10">
+        <h1 className=" text-center text-4xl">Books</h1>
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-7">
+          {data.map((data2) => (
+            <LoadedData key={data2.bookId} data2={data2}></LoadedData>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingData;
