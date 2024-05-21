70241901fc17531e6035cf02fde885cd15bc4330
OMI786I <sohrawardy1998@gmail.com>
2024-05-21 23:53:01 +0600

    done with banner section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div>
       <div>
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero min-h-screen bg-[#f2f2f2] md:px-16">
           <div className="hero-content flex-col lg:flex-row-reverse">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-              className="max-w-sm rounded-lg shadow-2xl"
-            />
+            <img src="pngwing-1.png" className="max-w-sm rounded-lg " />
             <div>
-              <h1 className="text-5xl font-bold">Box Office News!</h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-              <button className="btn btn-primary">Get Started</button>
+              <h1 className="text-5xl font-bold">
+                Books to freshen up your bookshelf
+              </h1>
+              <Link to="listedBooks">
+                <button className="btn btn-success text-white mt-16">
+                  View The List
+                </button>
+              </Link>
             </div>
           </div>
         </div>
