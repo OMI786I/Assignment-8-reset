93c51203ec85da08d7edbc582f6225010106b64b
OMI786I <sohrawardy1998@gmail.com>
2024-05-21 23:55:52 +0600

    banner responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,12 @@ const Home = () => {
   return (
     <div>
       <div>
-        <div className="hero min-h-screen bg-[#f2f2f2] md:px-16">
+        <div className="hero min-h-screen bg-[#f2f2f2] md:px-16 rounded-xl">
           <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src="pngwing-1.png" className="max-w-sm rounded-lg " />
+            <img
+              src="pngwing-1.png"
+              className=" max-w-xs md:max-w-sm rounded-lg "
+            />
             <div>
               <h1 className="text-5xl font-bold">
                 Books to freshen up your bookshelf
