8ab5974bc37336620118bef6a3456e856487b5b3
OMI786I <sohrawardy1998@gmail.com>
2024-05-22 17:05:47 +0600

    added sort button

--- src/components/DataShow.jsx
+++ src/components/DataShow.jsx
@@ -3,9 +3,31 @@ import "react-tabs/style/react-tabs.css";
 import ListedBooks from "./ListedBooks";
 import ReadBooks from "./ReadBooks";
 
+import "react-dropdown/style.css";
+
 const DataShow = () => {
   return (
     <div>
+      <h1 className="text-center text-2xl font-bold p-6 rounded-lg mb-9 bg-[#f4f4f4]">
+        Books
+      </h1>
+      <div className=" flex flex-row justify-center w-full  my-8">
+        {" "}
+        <details className="dropdown">
+          <summary className="m-1 btn btn-success text-white">
+            open or close
+          </summary>
+          <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </details>
+      </div>
+
       <Tabs>
         <TabList>
           <Tab>Read Books</Tab>
